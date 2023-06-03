73723b45dc8a80359b86b5592277e3823eeaee5d
Venura Warnasooriya <venurawarnasooriya@gmail.com>
2023-06-03 17:06:50 +0530

    sqare page and help page

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import Invitation from "./Components/Invitation";
 import Home from "./Components/Home";
 import Page3 from "./Components/Page3";
 import Page4 from "./Components/Page4";
+import Page6 from "./Components/Page6";
+import Help from "./Components/Help";
 
 function App() {
   return (
@@ -17,6 +19,8 @@ function App() {
           <Route exact path="/home" element={<Home />} />
           <Route exact path="/circle" element={<Page3 />} />
           <Route exact path="/triangle" element={<Page4 />} />
+          <Route exact path="/square" element={<Page6 />} />
+          <Route exact path="/help" element={<Help />} />
         </Routes>
       </Router>
     </Container>
